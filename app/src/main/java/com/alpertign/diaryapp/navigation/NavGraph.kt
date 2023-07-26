aadace6118be114d6a2316151f3158ac207c96aa
Alperen Acikgoz <alpertign@gmail.com>
2023-07-26 22:52:37 +0300

    -jwt mongodb auth added to authentication -logged in logic implemented to auth

--- app/src/main/java/com/alpertign/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/alpertign/diaryapp/navigation/NavGraph.kt
@@ -1,7 +1,17 @@
 package com.alpertign.diaryapp.navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -9,10 +19,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.alpertign.diaryapp.presentation.screens.auth.AuthenticationScreen
+import com.alpertign.diaryapp.presentation.screens.auth.AuthenticationViewModel
+import com.alpertign.diaryapp.util.Constants.APP_ID
 import com.alpertign.diaryapp.util.Constants.WRITE_SCREEN_ARGUEMENT_KEY
 import com.stevdzasan.messagebar.MessageBarState
 import com.stevdzasan.messagebar.rememberMessageBarState
 import com.stevdzasan.onetap.rememberOneTapSignInState
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Created by Alperen Acikgoz on 24,July,2023
@@ -25,28 +40,76 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
         navController = navController
     ) {
 
-        authenticationRoute()
+        authenticationRoute(
+            navigateToHome= {
+                navController.popBackStack()
+                navController.navigate(Screen.Home.route)
+            }
+        )
         homeRoute()
         writeRoute()
     }
 }
 
-fun NavGraphBuilder.authenticationRoute() {
+fun NavGraphBuilder.authenticationRoute(
+    navigateToHome: () -> Unit
+) {
     composable(route = Screen.Authentication.route) {
+        val viewModel : AuthenticationViewModel = viewModel()
+        val authenticated by viewModel.authenticated
+        val loadingState by viewModel.loadingState
         val oneTapState = rememberOneTapSignInState()
         val messageBarState = rememberMessageBarState()
         AuthenticationScreen(
-            loadingState = oneTapState.opened,
+            authenticated = authenticated,
+            loadingState = loadingState,
             messageBarState = messageBarState,
-            oneTapState = oneTapState
-        ) {
-            oneTapState.open()
-        }
+            oneTapState = oneTapState,
+            onButtonClicked = {
+                oneTapState.open()
+                viewModel.setLoading(true)
+            },
+            onTokenIdReceived = {tokenId ->
+                viewModel.signInWithMongoAtlas(
+                   tokenId = tokenId,
+                   onSuccess = {
+                       messageBarState.addSuccess("Successfully Authenticated!")
+                       viewModel.setLoading(false)
+                   },
+                   onError = {
+                       messageBarState.addError(it)
+                       viewModel.setLoading(false)
+                   }
+                )
+                //
+            },
+            onDialogDismissed = {message ->
+                messageBarState.addError(Exception(message))
+                viewModel.setLoading(false)
+            },
+            navigateToHome = navigateToHome
+        )
     }
 }
 
 fun NavGraphBuilder.homeRoute() {
     composable(route = Screen.Home.route) {
+        val scope = rememberCoroutineScope()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Button(onClick = {
+                scope.launch (Dispatchers.IO){
+                    App.create(APP_ID).currentUser?.logOut()
+                }
+            }) {
+                Text(text = "LOG OUT")
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/alpertign/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/alpertign/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import com.alpertign.diaryapp.util.Constants.CLIENT_ID
 import com.stevdzasan.messagebar.ContentWithMessageBar
 import com.stevdzasan.messagebar.MessageBarState
@@ -17,29 +18,43 @@ import com.stevdzasan.onetap.OneTapSignInWithGoogle
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
+    authenticated: Boolean,
     loadingState: Boolean,
     oneTapState: OneTapSignInState,
-    messageBarState : MessageBarState,
-    onButtonClicked: () -> Unit
-) {
+    messageBarState: MessageBarState,
+    onButtonClicked: () -> Unit,
+    onTokenIdReceived: (String) -> Unit,
+    onDialogDismissed: (String) -> Unit,
+    navigateToHome: () -> Unit,
+
+
+    ) {
     Scaffold(
         content = {
             ContentWithMessageBar(messageBarState = messageBarState) {
-                AuthenticationContent(loadingState, onButtonClicked)
-
+                AuthenticationContent(
+                    loadingState,
+                    onButtonClicked
+                )
             }
         }
     )
     OneTapSignInWithGoogle(
         state = oneTapState,
         clientId = CLIENT_ID,
-        onTokenIdReceived = {tokenId ->
-            Log.d("Auth", "tokenId : $tokenId")
-            messageBarState.addSuccess("Successfully Authenticated!")
+        onTokenIdReceived = { tokenId ->
+            onTokenIdReceived(tokenId)
         },
-        onDialogDismissed ={ message ->
-            Log.d("Auth", "message : $message")
-            messageBarState.addError(Exception(message))
+        onDialogDismissed = { message ->
+            onDialogDismissed(message)
         }
     )
+
+    LaunchedEffect(
+        key1 = authenticated,
+    ) {
+        if (authenticated) {
+            navigateToHome()
+        }
+    }
 }
